e1cd121a7171a7cebbd2b18724ffa91e34388f3a
odjuret03 <lucas.hornfeldt@gmail.com>
2026-04-26 09:14:53 +0200

    removed theme thing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,12 +8,6 @@ document.documentElement.classList.toggle(
 	localStorage.theme === "dark" ||
 	(!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
 );
-// Whenever the user explicitly chooses light mode
-localStorage.theme = "light";
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = "dark";
-// Whenever the user explicitly chooses to respect the OS preference
-localStorage.removeItem("theme");
 
 </script>
 
